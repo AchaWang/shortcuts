47136c4266c2672cab7ac93fd4a68110fcb736a5
AchaWang <achawang211@gmail.com>
2025-03-08 17:13:58 +0800

    Update ytdlp4.py

--- ytd/ytdlp4.py
+++ ytd/ytdlp4.py
@@ -1,4 +1,5 @@
 import yt_dlp
+import os
 import sys
 def ytdlp4(url,name):
   url = url
@@ -9,6 +10,16 @@ def ytdlp4(url,name):
   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download(url)
 if __name__=='__main__':
-  url=sys.argv[1]
-  name=sys.argv[2]
-  ytdlp4(url,name)
+  yt_lock = "yt.lock"
+  if os.path.exists(yt_lock):
+    os.remove(yt_lock)
+    print("got lock")
+    url=sys.argv[1]
+    name=sys.argv[2]
+    ytdlp4(url,name)
+    with open("yt.lock", "w") as file:
+      file.write("yt_dlp mission lock\n")
+    print("Lock returned successfully")
+  else:
+      print("No lock can get！")
+      print("Something went wrong")
