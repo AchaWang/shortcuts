d2751bf71e060849214918c295e40064d5550c3f
AchaWang <achawang211@gmail.com>
2025-03-09 01:26:37 +0800

    Delete ytd directory

--- ytd/createLock.py
+++
@@ -1,4 +0,0 @@
-if __name__=='__main__':
-    with open("./yt.lock", "w") as file:
-        file.write("yt_dlp mission lock\n")
-    print("Lock created successfully")
--- ytd/first_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip install -U yt-dlp
-python createLock.py
